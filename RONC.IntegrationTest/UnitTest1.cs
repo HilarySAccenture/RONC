7d56c15944a780e0fc261237c65cec39b8760111
Hilary Sasso-Schleh <hilary.sasso-schleh@accenture.com>
2019-05-20 10:56:27 -0400

    End to end test written

---
+++ RONC.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Shouldly;
+using Xunit;
+
+namespace RONC.IntegrationTest
+{
+    public class RONCShould
+    {
+        [Fact]
+        public void DisplayExpectedText()
+        {
+            var currentDirectory = Environment.CurrentDirectory;
+            var options = new FirefoxOptions();
+            options.AddArgument("--headless");
+            var fileName = "geckodrivermac";
+            var service = FirefoxDriverService.CreateDefaultService(currentDirectory, fileName);
+            var driver = new FirefoxDriver(service, options);
+
+            var result = string.Empty;
+            
+            try
+            {
+                driver.Navigate().GoToUrl("http://localhost:5000");
+                var page = driver.FindElementByTagName("pre");
+
+                result = page.Text;
+                
+            }
+            catch (WebDriverException wde)
+            {
+                
+            }
+            finally
+            {
+                driver.Quit();
+            }
+
+            result.ShouldContain("hello world");
+        }
+    }
+}
